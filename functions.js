6bd74c73b91b6ae5e958c7fc3206dbe4ec5d9eee
lighto <husseinnaim.github@gmail.com>
2020-11-09 18:03:07 -0500

    syntax

--- functions.js
+++ functions.js
@@ -1,14 +1,20 @@
 const offset ={
-    get_top:function(class_name){
-        var offset = $(class_name).offset().top
+    get_top:function(id_name){
+        var offset = document.getElementById(id_name).offsetTop ;
         return offset},
-    get_left:function(class_name){
-        var offset = $(class_name).offset().left 
+    get_left:function(id_name){
+        var offset = document.getElementById(id_name).offsetLeft;
         return offset
     },
-    get_offset:function(class_name){
-        var offset = $(class_name).offset();
-        return offset
+    get_offset:function(id_name){
+        var offset = Array(
+            document.getElementById(id_name).offsetTop,
+            document.getElementById(id_name).offsetLeft,
+            document.getElementById(id_name).offsetWidth,
+            document.getElementById(id_name).offsetHeight,
+        );
+
+        return 'TOP:'+ offset[0] +'\n' + 'LEFT:' + offset[1] + '\n' +'WIDTH:'+ offset[2] +'\n' + 'HEIGHT:' + offset[3] ;
     }
 }
 
